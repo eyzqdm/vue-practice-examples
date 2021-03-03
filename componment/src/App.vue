95659abd9d3b23e7e77ed326f582389486968cf5
郭庆 <31536879@qq.com>
2021-03-03 19:34:53 +0800

    update

--- componment/src/App.vue
+++ componment/src/App.vue
@@ -1,12 +1,36 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-13 10:13:44
+ * @LastEditTime: 2021-03-03 19:34:47
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \GVuec:\Users\Administrator\Desktop\Vue源码\新建文件夹\vue-practice-examples\componment\src\App.vue
+-->
 <template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <h3>{{num}}</h3>
+    <button @click="add">add</button>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return{
+      num:1
+    }
+  },
+  methods:{
+    add(){
+      this.num++
+    }
+  }
+}
+</script>
 
 <style>
 #app {
--- componment/tests/helle.spec.js
+++ componment/tests/helle.spec.js
@@ -1,7 +1,3 @@
 import { shallowMount } from '@vue/test-utils';
 import Hello from '@/views/slots/hello.vue'
 
-describe('Hello.vue',() => {
-
-    it('测试')
-})
